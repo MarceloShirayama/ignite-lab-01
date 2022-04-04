b57aa6169e484ccfd4cb3909c5977710cb042c2c
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-04-04 15:52:22 -0300

    (web) get token with SSP

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { useUser } from '@auth0/nextjs-auth0'
+import { getAccessToken, useUser } from '@auth0/nextjs-auth0'
+import { GetServerSideProps } from 'next'
 
 export default function Home() {
   const { user } = useUser()
@@ -10,4 +11,13 @@ export default function Home() {
       <a href="/api/auth/login">Login</a>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const token = getAccessToken(req, res)
+  console.log(token)
+
+  return {
+    props: {}
+  }
 }
