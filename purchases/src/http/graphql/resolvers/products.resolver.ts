676f30af1e67130615214fde8fe2a0fc7b4bf06a
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-04-05 15:24:39 -0300

    (purchases): add feature create product

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -1,7 +1,8 @@
-// import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
-// import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 import { ProductsService } from 'src/services/products.service';
+import { CreateProductInput } from '../inputs/create-product-input';
 import { Product } from '../models/product';
 
 @Resolver()
@@ -10,7 +11,13 @@ export class ProductsResolver {
 
   @Query(() => [Product])
   // @UseGuards(AuthorizationGuard)
-  products() {
+  listProducts() {
     return this.service.findAll();
+  }
+
+  @Mutation(() => Product)
+  @UseGuards(AuthorizationGuard)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.service.create(data);
   }
 }
--- purchases/src/services/products.service.ts
+++ purchases/src/services/products.service.ts
@@ -2,11 +2,48 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 import { Product } from 'src/http/graphql/models/product';
 
+type createProductInput = Pick<Product, 'title'>;
+
+function stringToSlug(str: string, separator: string): string {
+  str = str.trim();
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  const from = 'åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;';
+  const to = 'aaaaaaeeeeiiiioooouuuunc------';
+
+  for (let i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  return str
+    .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-') // collapse dashes
+    .replace(/^-+/, '') // trim - from start of text
+    .replace(/-+$/, '') // trim - from end of text
+    .replace(/-/g, separator);
+}
+
 @Injectable()
 export class ProductsService {
   constructor(private readonly prisma: PrismaService) {}
 
   findAll(): Promise<Product[]> {
     return this.prisma.product.findMany();
+  }
+
+  async create({ title }: createProductInput): Promise<Product> {
+    const slug = stringToSlug(title, '-');
+
+    const alreadyExists = await this.prisma.product.findUnique({
+      where: { slug },
+    });
+
+    if (alreadyExists) {
+      throw new Error('another product with same slug already exists');
+    }
+
+    return this.prisma.product.create({ data: { title, slug } });
   }
 }
