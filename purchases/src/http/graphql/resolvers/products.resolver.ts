f1157cb9417abb4358af1b7bf582299ec6c69de6
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-04-05 13:35:56 -0300

    (pruchases): make the route products public

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -1,6 +1,6 @@
-import { UseGuards } from '@nestjs/common';
+// import { UseGuards } from '@nestjs/common';
 import { Query, Resolver } from '@nestjs/graphql';
-import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+// import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 import { ProductsService } from 'src/services/products.service';
 import { Product } from '../models/product';
 
@@ -9,7 +9,7 @@ export class ProductsResolver {
   constructor(private service: ProductsService) {}
 
   @Query(() => [Product])
-  @UseGuards(AuthorizationGuard)
+  // @UseGuards(AuthorizationGuard)
   products() {
     return this.service.findAll();
   }
