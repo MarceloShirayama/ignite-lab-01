b42c4605be9bd598440b6545a166cc22822f84c8
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-04-06 16:09:37 -0300

    (classroom): add mutation create course

--- classroom/src/services/courses.service.ts
+++ classroom/src/services/courses.service.ts
@@ -1,6 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 
+type CreateCourseParams = { title: string };
+
+function stringToSlug(str: string, separator: string): string {
+  str = str.trim();
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  const from = 'åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;';
+  const to = 'aaaaaaeeeeiiiioooouuuunc------';
+
+  for (let i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  return str
+    .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-') // collapse dashes
+    .replace(/^-+/, '') // trim - from start of text
+    .replace(/-+$/, '') // trim - from end of text
+    .replace(/-/g, separator);
+}
+
 @Injectable()
 export class CoursesService {
   constructor(private prisma: PrismaService) {}
@@ -11,5 +34,19 @@ export class CoursesService {
 
   findById(id: string) {
     return this.prisma.course.findUnique({ where: { id } });
+  }
+
+  async create({ title }: CreateCourseParams) {
+    const slug = stringToSlug(title, '-');
+
+    const courseAlreadyExists = await this.prisma.course.findFirst({
+      where: { slug },
+    });
+
+    if (courseAlreadyExists) {
+      throw new Error(`Course with title ${title} already exists`);
+    }
+
+    return this.prisma.course.create({ data: { title, slug } });
   }
 }
