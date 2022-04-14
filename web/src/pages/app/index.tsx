e9e1318ca9fd177d34e678a4e586d3365edf349c
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-04-14 17:10:42 -0300

    (web): use  getServerProps to get products

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -3,17 +3,20 @@ import {
   useUser,
   withPageAuthRequired
 } from '@auth0/nextjs-auth0'
-import { useGetProductsQuery } from '../../graphql/generated/graphql'
+import {
+  getServerPageGetProducts,
+  ssrGetProducts
+} from '../../graphql/generated/page'
 import { withApollo } from '../../lib/withApollo'
 
-function Home() {
+function Home({ data }) {
   const { user } = useUser()
-  const { data, loading, error } = useGetProductsQuery()
+  // const { data, loading, error } = useGetProductsQuery()
 
   return (
     <div>
       <h1>Hello Ignite Lab</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>{JSON.stringify(data.products, null, 2)}</pre>
       <pre>{JSON.stringify(user, null, 2)}</pre>
       <a href="/api/auth/logout">Logout</a>
     </div>
@@ -21,13 +24,11 @@ function Home() {
 }
 
 export const getServerSideProps = withPageAuthRequired({
-  getServerSideProps: async ({ req, res }) => {
-    console.log(getAccessToken(req, res))
+  getServerSideProps: async (ctx) => {
+    console.log(await getAccessToken(ctx.req, ctx.res))
 
-    return {
-      props: {}
-    }
+    return getServerPageGetProducts(null, ctx)
   }
 })
 
-export default withApollo(Home)
+export default withApollo(ssrGetProducts.withPage()(Home))
